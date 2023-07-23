70efbb6f3e461d72c1b4a465e6bcd0bb9adbbae5
Borimino <MarkusAndreassen1994@gmail.com>
2023-07-23 13:53:18 +0200

    Decent alpha-beta pruned bot fighting against non-pruned bot

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,5 +1,7 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChessChallenge.Example
 {
@@ -7,6 +9,146 @@ namespace ChessChallenge.Example
     // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
+        // BOT START
+        // DECENT VERSION OF MY BOT
+        // Piece values: null, pawn, knight, bishop, rook, queen, king
+        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+        // Bonus for a Check
+        int checkBonus = 100;
+        // Bonus for a CheckMate
+        int checkMateBonus = 1000000;
+        // Start depth
+        int startDepth = 3;
+
+        // Randomness
+        Random rng = new Random();
+
+        public Move Think(Board board, Timer timer)
+        {
+            //int numberOfPieces = board.GetAllPieceLists().Aggregate(0, (acc, piece) => acc + piece.Count);
+            int numberOfEnemyPieces = board.GetAllPieceLists().Where((piece) => piece.IsWhitePieceList != board.IsWhiteToMove).Select(piece => piece.Count).Sum();
+            int depth = startDepth;
+            if (numberOfEnemyPieces < 3)
+            {
+                depth += 2;
+            }
+            /*
+            int depth = startDepth;
+            if (numberOfPieces < 16)
+            {
+                depth += 1;
+            }
+            if (numberOfPieces < 8)
+            {
+                depth += 1;
+            }
+            */
+            //System.Console.WriteLine("Starting depth: " + depth);
+            ScoredMove scoredMove = PickBestMove(board, depth, int.MinValue, int.MaxValue);
+            //System.Console.WriteLine("Score for this move was " +  scoredMove.score);
+            return scoredMove.move;
+        }
+
+        private ScoredMove PickBestMove(Board board, int depth, int alpha, int beta)
+        {
+            Move[] moves = board.GetLegalMoves();
+
+            if (moves.Length == 0)
+            {
+                //System.Console.WriteLine("No more moves to make at position " + board.GetFenString());
+                return new ScoredMove(new Move(), -1 * checkMateBonus);
+            }
+
+            ScoredMove bestMove = new ScoredMove(moves[0], int.MinValue);
+            List<ScoredMove> bestMoves = new List<ScoredMove> { bestMove };
+            foreach (Move move in moves)
+            {
+                int score;
+                Boolean shouldBreak = false;
+                board.MakeMove(move);
+                if (depth == 0) // If we are stopping the search, return the score of the player, who just made a move
+                {
+                    int tmpScore = -1 * Score(board);
+                    /*if (tmpScore != 0)
+                    {
+                        System.Console.WriteLine("Score at depth 0: " + tmpScore);
+                    }*/
+                    score = tmpScore;
+                }
+                else
+                {
+                    // Get the score of the best move our opponent could make, and return -1 * that
+                    score = PickBestMove(board, depth - 1, -beta, -alpha).score;
+                    /*if (score > beta)
+                    {
+                        shouldBreak = true;
+                    }
+                    alpha = Math.Max(alpha, score);*/
+                    score *= -1;
+                }
+                if (board.IsInCheck())
+                {
+                    score += checkBonus;
+                }
+                if (board.IsInCheckmate())
+                {
+                    score += checkMateBonus;
+                    //System.Console.WriteLine("Found a checkmate with score " + score + " at position " + board.GetFenString());
+                }
+                board.UndoMove(move);
+                if (score == bestMove.score)
+                {
+                    bestMoves.Add(new ScoredMove(move, score));
+                }
+                if (score > bestMove.score)
+                {
+                    bestMove = new ScoredMove(move, score);
+                    bestMoves = new List<ScoredMove> { bestMove };
+                }
+                if (shouldBreak)
+                {
+                    break;
+                }
+            }
+
+            /*if (bestMove.score != 0 && depth != 0)
+            {
+                System.Console.WriteLine("Score at depth " + depth + ": " + bestMove.score);
+            }*/
+
+            return bestMoves[rng.Next(bestMoves.Count)];
+            //return bestMoves[0];
+        }
+
+        // Goes through each of the Piece Lists,
+        // if it is the same color as whoever is about to move, add the value * count to the score,
+        // if it is the other color, subtract the value * count from the score
+        private int Score(Board board)
+        {
+            int score = 0;
+            PieceList[] pieces = board.GetAllPieceLists();
+            foreach(PieceList piece in pieces)
+            {
+                int sign = piece.IsWhitePieceList ? (board.IsWhiteToMove ? 1 : -1) : (board.IsWhiteToMove ? -1 : 1);
+                score += sign * pieceValues[(int) piece.TypeOfPieceInList] * piece.Count;
+            }
+            return score;
+        }
+
+        private class ScoredMove
+        {
+            public Move move;
+            public int score;
+
+            public ScoredMove(Move move, int score)
+            {
+                this.move = move;
+                this.score = score;
+            }
+        }
+        // BOT END
+        /*
+        // EvilBot START
         // Piece values: null, pawn, knight, bishop, rook, queen, king
         int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
 
@@ -50,5 +192,6 @@ namespace ChessChallenge.Example
             board.UndoMove(move);
             return isMate;
         }
+        */
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,104 +5,117 @@ using System.Linq;
 
 public class MyBot : IChessBot
 {
-    bool goodMoves = false;
-
     // Piece values: null, pawn, knight, bishop, rook, queen, king
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
     // Bonus for a Check
-    int checkBonus = 20000;
+    int checkBonus = 100;
+    // Bonus for a Capture
+    int captureBonus = 10;
     // Bonus for a CheckMate
-    int checkMateBonus = 100000;
+    int checkMateBonus = 1000000;
+    // Mallus for a Draw
+    int drawMallus = 1000;
+
+
     // Start depth
-    int startDepth = 3;
+    int startDepth = 5;
 
     // Randomness
     Random rng = new Random();
 
     public Move Think(Board board, Timer timer)
     {
-        int numberOfPieces = board.GetAllPieceLists().Aggregate(0, (acc, piece) => acc + piece.Count);
-        //int depth = startDepth;
+        int numberOfEnemyPieces = board.GetAllPieceLists().Where((piece) => piece.IsWhitePieceList != board.IsWhiteToMove).Select(piece => piece.Count).Sum();
         int depth = startDepth;
-        if (numberOfPieces < 16)
+        if (numberOfEnemyPieces < 3)
         {
-            depth += 1;
-        }
-        if (numberOfPieces < 8)
-        {
-            depth += 1;
+            depth += 2;
         }
         System.Console.WriteLine("Starting depth: " + depth);
-        ScoredMove scoredMove = PickBestMove(board, depth);
-        //System.Console.WriteLine("Score for this move was " +  scoredMove.score);
+        ScoredMove scoredMove = PickBestMove(board, depth, int.MinValue + 10, int.MaxValue - 10, Score(board));
+        System.Console.WriteLine("Score for this move was " +  scoredMove.score);
+        System.Console.WriteLine("This is an improvement of " + (scoredMove.score - Score(board)));
         return scoredMove.move;
     }
 
-    private ScoredMove PickBestMove(Board board, int depth)
+    private ScoredMove PickBestMove(Board board, int depth, int myMinimumAssuredScore, int opponentsMaximumAssuredScore, int baseScore)
     {
-        Move[] moves = board.GetLegalMoves();
+        List<Move> allMoves = board.GetLegalMoves().ToList<Move>();
+        List<Move> capturingMoves = allMoves.Where((move) => move.IsCapture).ToList<Move>();
+        List<Move> nonCapturingMoves = allMoves.Where((move) =>  !move.IsCapture).ToList<Move>();
+        List<Move> moves = capturingMoves.Concat(nonCapturingMoves).ToList<Move>();
 
-        if (moves.Length == 0)
+        if (moves.Count == 0)
         {
-            //System.Console.WriteLine("No more moves to make");
-            return new ScoredMove(new Move(), int.MinValue);
+            return new ScoredMove(new Move(), -checkMateBonus);
         }
 
-        ScoredMove bestMove = new ScoredMove(moves[0], goodMoves ? int.MinValue : int.MaxValue);
+        ScoredMove bestMove = new ScoredMove(moves[0], int.MinValue);
         List<ScoredMove> bestMoves = new List<ScoredMove> { bestMove };
         foreach (Move move in moves)
         {
             int score;
             board.MakeMove(move);
-            if (depth == 0)
+            if (depth == 0) // If we are stopping the search, return the score of the player, who just made a move
             {
-                int tmpScore = Score(board);
-                /*if (tmpScore != 0)
-                {
-                    System.Console.WriteLine("Score at depth 0: " + tmpScore);
-                }*/
+                int tmpScore = -Score(board);
                 score = tmpScore;
             }
             else
             {
-                score = -1 * PickBestMove(board, depth - 1).score;
+                // Get the score of the best move our opponent could make, and return -1 * that
+                score = -PickBestMove(board, depth - 1, -opponentsMaximumAssuredScore, -myMinimumAssuredScore, baseScore).score;
+            }
+            if (move.IsCapture)
+            {
+                score += captureBonus;
             }
             if (board.IsInCheck())
             {
-                score += goodMoves ? checkBonus : -1 * checkBonus;
+                score += checkBonus;
             }
             if (board.IsInCheckmate())
             {
-                score += goodMoves ? checkMateBonus : -1 * checkMateBonus;
+                score += checkMateBonus;
+            }
+            if (board.IsDraw())
+            {
+                score -= drawMallus;
             }
             board.UndoMove(move);
-            if (score == bestMove.score)
+            /*if (score == bestMove.score)
             {
                 bestMoves.Add(new ScoredMove(move, score));
-            }
-            if (goodMoves ? score > bestMove.score : score < bestMove.score)
+            }*/
+            if (score > bestMove.score)
             {
                 bestMove = new ScoredMove(move, score);
                 bestMoves = new List<ScoredMove> { bestMove };
             }
+            if (score > myMinimumAssuredScore)
+            {
+                myMinimumAssuredScore = score;
+            }
+            if (opponentsMaximumAssuredScore <= myMinimumAssuredScore)
+            {
+                return bestMove;
+            }
         }
-
-        /*if (bestMove.score != 0 && depth != 0)
-        {
-            System.Console.WriteLine("Score at depth " + depth + ": " + bestMove.score);
-        }*/
 
         return bestMoves[rng.Next(bestMoves.Count)];
     }
 
+    // Goes through each of the Piece Lists,
+    // if it is the same color as whoever is about to move, add the value * count to the score,
+    // if it is the other color, subtract the value * count from the score
     private int Score(Board board)
     {
         int score = 0;
         PieceList[] pieces = board.GetAllPieceLists();
         foreach(PieceList piece in pieces)
         {
-            int multiplier = piece.IsWhitePieceList ? (board.IsWhiteToMove ? 1 : -1) : (board.IsWhiteToMove ? -1 : 1);
-            score += multiplier * pieceValues[(int) piece.TypeOfPieceInList] * piece.Count;
+            int sign = piece.IsWhitePieceList ? (board.IsWhiteToMove ? 1 : -1) : (board.IsWhiteToMove ? -1 : 1);
+            score += sign * pieceValues[(int) piece.TypeOfPieceInList] * piece.Count;
         }
         return score;
     }
